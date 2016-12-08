37819857fc41f00922e53edf21f7f4e35a983c91
yscherba@speroteck.com <yscherba@speroteck.com>
2016-12-08 10:20:13 +0200

    gulp with live reload of css and html

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # AND
-Angular Note Wrangler (code school course)
-http://campus.codeschool.com/courses/staying-sharp-with-angular-js
+Note Wrangler
 Project for learning Angular
+goals:
+1. Set up Gulp Server for angular app
+2. Create App
+    2.1 Routing
+    2.2 Services
+    2.3 Directives
+    2.4 API
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    gutil = require('gulp-util'),
+    jshint = require('gulp-jshint'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat'),
+    clean = require('gulp-clean'),
+    embedlr = require('gulp-embedlr'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        open: true,
+        notify: false
+    });
+});
+gulp.task('lint', function() {
+    gulp.src('./app/scripts/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('browserify', function() {
+    gulp.src(['app/scripts/main.js'])
+        .pipe(browserify({
+            insertGlobals: true,
+            debug: true
+        }))
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+gulp.task('watch', ['lint', 'sass'], function() {
+    browserSync.init({
+        server: "dist"
+    });
+    gulp.watch(['app/scripts/*.js', 'app/scripts/**/*.js'],[
+        'lint',
+        'browserify'
+    ]);
+    gulp.watch('./app/styles/**/*.scss', ['sass']);
+    gulp.watch(['app/index.html', 'app/views/**/*.html'], ['views']).on('change', browserSync.reload);
+});
+gulp.task('views', function() {
+    gulp.src('app/index.html').pipe(gulp.dest('dist/'));
+    gulp.src('./app/views/**/*')
+        .pipe(gulp.dest('dist/views/'));
+});
+
+gulp.task('sass', function () {
+    gulp.src('app/styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(browserSync.stream());
+});
+// Dev task
+gulp.task('dev', function() {
+    gulp.start('watch');
+});
